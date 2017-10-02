24dc7667441ea6aeb73058929ba551b7594f0616
DESKTOP-DGHE2M7\Javier Esponda <javieresponda1995@gmail.com>
2017-10-02 14:52:36 -0500

    Tarea Mesh, Shader y ShaderProgram

---
+++ Project1/Shader.cpp
@@ -0,0 +1,29 @@
+#include <Shader.h>
+#include <InputFile.h>
+
+Shader::Shader() {
+	_shaderHandle = 0;
+}
+
+Shader::~Shader() {
+	glDeleteShader(_shaderHandle);
+}
+
+void Shader::CreateShader(std::string path, GLenum type) {
+	if (_shaderHandle != 0) {
+		glDeleteShader(_shaderHandle);
+	}
+
+	InputFile ifile;
+	ifile.Read(path);
+	std::string vertexSource = ifile.GetContents();
+
+	_shaderHandle = glCreateShader(type);
+	const GLchar *vertexSource_c = (const GLchar*)vertexSource.c_str();
+	glShaderSource(_shaderHandle, 1, &vertexSource_c, nullptr);
+	glCompileShader(_shaderHandle);
+}
+
+GLuint Shader::GetHandle() {
+	return _shaderHandle;
+}
---
+++ Project1/Shader.h
@@ -0,0 +1,16 @@
+#include <string>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+#include <vector>
+
+class Shader {
+public:
+	Shader();
+	~Shader();
+	void CreateShader(std::string path, GLenum type);
+	GLuint GetHandle();
+
+private:
+	GLuint _shaderHandle;
+}
