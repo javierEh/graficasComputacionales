02fbd7427ae70848ded210184b257a1b66d498e0
javierEh <javieresponda1995@gmail.com>
2017-10-02 15:19:49 -0500

    Update Mesh.cpp
    Correccion

--- Project1/Mesh.cpp
+++ Project1/Mesh.cpp
@@ -1,14 +1,14 @@
-#include <Mesh.h>
+#include "Mesh.h"
 
-Mesh::Mesh() {//El constructor inicializa los valores por default de las variables.
+Mesh::Mesh(){//El constructor inicializa los valores por default de las variables.
 	_vertexArrayObject = 0;
-	_positionsVertexBufferObject = 0;
+	_positionsVertexBufferObject= 0;
 	_colorsVertexBufferObject = 0;
 	_vertexCount = 0;
 
 }
 
-Mesh::~Mesh() {
+Mesh::~Mesh(){
 	//El destructor borra el VAO y todos los VBOs. (Opcional +2 puntos)
 	//Tenemos que borrar el vao, las posiciones y los colores
 	glDeleteVertexArrays(1, &_vertexArrayObject);
@@ -17,50 +17,50 @@ Mesh::~Mesh() {
 	glBindVertexArray(0);
 }
 
-void Mesh::CreateMesh(GLint vertexCount) {
+void Mesh::CreateMesh(GLint vertexCount){
 	_vertexCount = vertexCount;
 	glGenVertexArrays(1, &_vertexArrayObject);
 	glBindVertexArray(_vertexArrayObject);
 
 }
 
-void Mesh::Draw(GLenum primitive) {
-	glDrawArrays(primitive, 0, vertexCount);
+void Mesh::Draw(GLenum primitive){
+	glDrawArrays(primitive, 0, _vertexCount);
 }
 
-void Mesh::SetPositionAttribute(std::vector<glm::vec2> positions, GLenum usage, GLuint locationIndex) {
-	if (positions.size() == 0 || positions.size() != vertexCount) {
+void Mesh::SetPositionAttribute(std::vector<glm::vec2> positions, GLenum usage, GLuint locationIndex){
+	if (positions.size() == 0 || positions.size() != _vertexCount){
 		return;
-	}
+	}	
 
 	SetAttributeData(_positionsVertexBufferObject, sizeof(glm::vec2) * positions.size(), positions.data(), usage, locationIndex, 2);
 }
 
-void Mesh::SetPositionAttribute(std::vector<glm::vec3> positions, GLenum usage, GLuint locationIndex) {
-	if (positions.size() == 0 || positions.size() != vertexCount) {
+void Mesh::SetPositionAttribute(std::vector<glm::vec3> positions, GLenum usage, GLuint locationIndex){
+	if (positions.size() == 0 || positions.size() != _vertexCount){
 		return;
-	}
+	}	
 
 	SetAttributeData(_positionsVertexBufferObject, sizeof(glm::vec3) * positions.size(), positions.data(), usage, locationIndex, 3);
 }
 
-void Mesh::SetColorAttribute(std::vector<glm::vec3> colors, GLenum usage, GLuint locationIndex) {
-	if (colors.size() == 0 || colors.size() != _vertexCount) {
+void Mesh::SetColorAttribute(std::vector<glm::vec3> colors, GLenum usage, GLuint locationIndex){
+	if (colors.size() == 0 || colors.size() != _vertexCount){
 		return;
 	}
 	SetAttributeData(_colorsVertexBufferObject, sizeof(glm::vec3) * colors.size(), colors.data(), usage, locationIndex, 3);
 }
 
 void Mesh::SetColorAttribute(std::vector<glm::vec4> colors, GLenum usage, GLuint locationIndex) {
-	if (colors.size() == 0 || colors.size() != _vertexCount) {
+	if (colors.size() == 0 || colors.size() != _vertexCount){
 		return;
 	}
 	SetAttributeData(_colorsVertexBufferObject, sizeof(glm::vec4) * colors.size(), colors.data(), usage, locationIndex, 4);
 }
 
-void Mesh::SetAttributeData(GLuint & buffer, const GLsizeiptr size, const void * data, GLenum usage, GLuint locationIndex, const GLint components) {
+void Mesh::SetAttributeData(GLuint& buffer, const GLsizeiptr size, const void * data, GLenum usage, GLuint locationIndex, const GLint components){
 	glDeleteBuffers(1, &buffer);
-	glGenBuffer(1, &buffer);
+	glGenBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
 	glBufferData(GL_ARRAY_BUFFER, size, data, usage);
 	glEnableVertexAttribArray(locationIndex);
